c007d3d7ed25b01920babb0b6142c62cffbe8856
Pablo <2005pcgv@gmai.com>
2024-10-16 19:54:54 -0600

    tarea2/unidad3

---
+++ HOSPITAL IMSS/src/Main.java
@@ -0,0 +1,13 @@
+import hospital.Hospital;
+import menu.Menu;
+
+
+public class Main {
+    public static void main(String[] args) {
+// user: A1
+// contra:1234
+        Menu menu = new Menu();
+        menu.login();
+
+    }
+}
