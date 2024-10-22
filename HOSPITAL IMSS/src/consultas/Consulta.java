d8ccdc7d2a8de86326464c92627732bcce63563b
Pablo <2005pcgv@gmai.com>
2024-10-22 16:23:12 -0600

    tarea4/unidad3

---
+++ HOSPITAL IMSS/src/consultas/Consulta.java
@@ -0,0 +1,59 @@
+package consultas;
+
+import consultas.utils.Status;
+import usuarios.medicos.Medico;
+import usuarios.pacientes.Paciente;
+import consultorios.Consultorio;
+import java.time.LocalDateTime;
+
+public class Consulta {
+    public String id;
+    public LocalDateTime fechaHora;
+    public Paciente paciente;
+    public Medico medico;
+    public Consultorio consultorio;
+    public Status status;
+
+    public Consulta(String id, LocalDateTime fechaHora, Paciente paciente, Medico medico, Consultorio consultorio) {
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.consultorio = consultorio;
+        this.status = Status.PENDIENTE;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String mostrarDatos(){
+        String datos = String.format("ID: %s, FECHA/HORA: %s, ID PACIENTE: %s, PACIENTE: %s, ID MEDICO: %s, MEDICO: %s, PISO CONSULTRIO: %d, CONSULTORIO: %d", this.id, this.fechaHora, this.paciente.getId(), this.paciente.getNombre(), this.medico.getId(), this.medico.getNombre(), this.consultorio.getPiso(), this.consultorio.getNumeroConsultorio());
+        return datos;
+    }
+}
---
+++ HOSPITAL IMSS/src/usuarios/administrador/Administrador.java
@@ -0,0 +1,26 @@
+package usuarios.administrador;
+
+import usuarios.Usuario;
+import usuarios.utils.Rol;
+
+import java.time.LocalDate;
+
+public class Administrador extends Usuario {
+    public Double sueldo;
+    public String rfc;
+    public int aniosTrabajados;
+
+    public Administrador(String id, String nombre, String apellidos, LocalDate fechaNacimiento, String telefono, String contrasenia, Rol rol, Double sueldo, String rfc, int aniosTrabajados) {
+        super(id, nombre, apellidos, fechaNacimiento, telefono, contrasenia, rol);
+        this.sueldo = sueldo;
+        this.rfc = rfc;
+        this.aniosTrabajados = aniosTrabajados;
+    }
+
+    public String mostrarInformacion() {
+        String datos = String.format("ID: %s, NOMBRE: %s, APELLIDOS: %s, FECHA NACIMIENTO: %s, TELEFONO: %s, SUELDO: $%.2f, RFC: %s, ANIOS TRABAJADOS: %d",
+             this.id,this.nombre, this.apellidos, this.fechaNacimiento, this.telefono, this.sueldo, this.rfc, this.aniosTrabajados);
+        return datos;
+    }
+
+}
